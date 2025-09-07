654164389b50ac0c75f1e4eb2971aac69f41ae6f
Dorian Bucur <sirdorius@gmail.com>
2025-09-07 22:25:06 +0200

    update build scripts and docs

--- build_desktop.sh
+++ build_desktop.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -eu
 
+# TODO build scripts are a bit of a mess. Use just instead and update vscode tasks
 OUT_DIR="build/desktop"
 mkdir -p $OUT_DIR
-odin build source/main_desktop -vet -strict-style -out:$OUT_DIR/game_desktop.bin
+odin build source/main_desktop -out:$OUT_DIR/game_desktop.bin
 cp -R ./assets/ ./$OUT_DIR/assets/
 echo "Desktop build created in ${OUT_DIR}"
---
+++ readme.md
@@ -0,0 +1,2 @@
+## Dev
+Note: Project is configured to work on linux and vscode. Scripts for win and sublime are kept around from the template but might be out of date
